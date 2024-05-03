58b6cb6b0e408612e77291f637f38652ede1424b
vincentrahadianutama <vincentutama38@smk.belajar.id>
2024-05-03 15:04:03 +0700

    Tugas React JS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="box">
+        <h1 className="title">Ahmad Sibgatullah L</h1>
+        <hr></hr>
+        <p className="paragraph">
+          Saya dari Kelas 12 sudah berhasil menginstall React JS
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
